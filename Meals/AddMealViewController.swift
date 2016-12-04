56fd3a30d63038f57bcf67409c591182e4ea9a7f
Matt Deuschle <mdeuschle@gmail.com>
2016-12-04 14:36:37 -0800

    logic for adding a new meal

--- Meals/AddMealViewController.swift
+++ Meals/AddMealViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol AddMealDelegate {
+    func addMeal(meal: Meal)
+}
+
 class AddMealViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var addMealTextField: UITextField!
@@ -18,6 +22,8 @@ class AddMealViewController: UIViewController, UITableViewDelegate, UITableViewD
                  Item(name: "Mayo", calories: 20),
                  Item(name: "Peppers", calories: 4)
     ]
+
+    var addMealDelegate: AddMealDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +43,20 @@ class AddMealViewController: UIViewController, UITableViewDelegate, UITableViewD
         return cell
      }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+        }
+    }
+
     @IBAction func addButtonTapped(_ sender: UIButton) {
+
+        if let mealName = addMealTextField.text, let mealHappiness = addHappinessTextField.text, let delegate = addMealDelegate {
+            if let happinessInt = Int(mealHappiness) {
+                let meal = Meal(name: mealName, happiness: happinessInt)
+                delegate.addMeal(meal: meal)
+            }
+        }
 
         if let navigation = navigationController {
             navigation.popViewController(animated: true)
--- Meals/MealTableViewController.swift
+++ Meals/MealTableViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class MealTableViewController: UITableViewController {
+
+class MealTableViewController: UITableViewController, AddMealDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,19 @@ class MealTableViewController: UITableViewController {
                  Meal(name: "Porterhouse Steak", happiness: 9),
                  Meal(name: "Toast", happiness: 5)
     ]
+
+    func addMeal(meal: Meal) {
+        meals.append(meal)
+        tableView.reloadData()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addMealSegue" {
+            if let dvc = segue.destination as? AddMealViewController {
+                dvc.addMealDelegate = self
+            }
+        }
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return meals.count
